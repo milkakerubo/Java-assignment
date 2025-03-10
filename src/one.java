c84d831db572298277aba0371dc195a471bbce63
milkakerubo <160165700+milkakerubo@users.noreply.github.com>
2025-03-10 12:33:35 +0300

    Update one.java

--- src/one.java
+++ src/one.java
@@ -1,13 +1,11 @@
 public class one {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
-
-        String name="Milka";
+        String name = scanner.nextLine();
         System.out.print("Enter your age: ");
-
-        int age=20;
+        int age = scanner.nextInt();
         System.out.println("Hello " + name + ", you are " + age + " years old!");
+        scanner.close();
     }
 }
-
-
